88c68a69b4be806a46c77aecd5d87dcdc6fe5897
dmunofer <diego.fmz@hotmail.com>
2022-03-24 09:08:20 +0100

    ej4 transferir falla ingresar

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,5 +1,5 @@
 class Cuenta_Bank():
-    def __init__(self,id,titular,fecha,num_cuenta,saldo):
+    def __init__(self,id=int,titular=str,fecha=int,num_cuenta=float,saldo=float):
         self.id = id
         self.titular = titular
         self.fecha = fecha
@@ -9,6 +9,18 @@ class Cuenta_Bank():
     def retirar_dinero(self, num_retirar):
         if self.saldo > num_retirar:
             self.saldo = self.saldo - num_retirar
-    
+        else:
+            raise Exception("No tienes suficiente saldo")
+
     def ingresar_dinero(self, num_ingresar):
         self.saldo = self.saldo + num_ingresar
+
+    def transferir_dinero(self, cuenta2, cantidad):
+        try:
+            self.retirar_dinero(cantidad)
+            cuenta2.ingresar_dinero(cantidad)
+        except:
+            raise Exception("No tienes suficiente saldo")
+
+
+
